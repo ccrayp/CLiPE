9ed0f7bb7db66facd113a86f06bbd7dccbcef9d2
ccrayp <demixhd2@gmail.com>
2026-05-27 17:14:18 +0300

    fix

--- server/api/internal/service/handler.go
+++ server/api/internal/service/handler.go
@@ -62,10 +62,10 @@ func (h *ServiceHandler) Filter(ctx *gin.Context) {
 	}
 
 	utils.RespondSuccess(ctx, http.StatusOK, nil, gin.H{
-		"users":  data,
-		"limit":  limit,
-		"offset": offset,
-		"count":  count,
+		"services": data,
+		"limit":    limit,
+		"offset":   offset,
+		"count":    count,
 	})
 }
 
